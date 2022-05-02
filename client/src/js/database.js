dcdc775147432adb20fa4e17e7ddf502359208b2
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-05-01 20:16:29 -0500

    initialized npms

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,17 +16,18 @@ const initdb = async () =>
   We need to add some code below which will take updated content and save it to IndexedDB.
 */
 export const putDb = async (content) => {
+  console.log("PUT to database");
   // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
-  const DB_VAR = await openDB('DB_NAME', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Now create a variable for the transaction
-  const TX_VAR = jateDb.transaction('DB_NAME', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Now create a variable for the store
-  const STORE_VAR = tx.objectStore('DB_NAME');
+  const STORE_VAR = tx.objectStore('jate');
 
   // Now create a variable named "request" and have it perform the update
-  const VAR_NAME = store.put({ id: 1, value: content });
+  const request = store.put({ id: 1, value: content });
 
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
